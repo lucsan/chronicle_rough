88f2ec3ed9c8e6e72244e2add7fff83084533cdc
lucsan <Luke.Tennant@gmail.com>
2019-01-13 14:09:01 +0000

    Refactored and added camel to desc.

--- app/components/tools/textify.js
+++ app/components/tools/textify.js
@@ -1,9 +1,31 @@
 const camelToTitle = (camelCase) => {
+  let chars = camelAddSpaces(camelCase)
+  let text = charsToText(chars)
+  text = text.charAt(0).toUpperCase() + text.slice(1)
+  return text
+}
+
+const camelToDesc = (camelCase) => {
+  let chars = camelAddSpaces(camelCase)
+  let text = charsToText(chars)
+  text = text.toLowerCase()
+  text = text.charAt(0).toUpperCase() + text.slice(1)
+  return text
+}
+
+const charsToText = (chars) => {
+  let text = ''
+  for (let i = 0; i < chars.length; i++) { text += `${chars[i]}` }
+  return text
+}
+
+const camelAddSpaces = (camelCase) => {
   let chars = camelCase.split('')
-  let t = ''
-  for (let i in chars) {
-    if (chars[i] === chars[i].toUpperCase()) t += ' '
-    t += chars[i]
+  let a = []
+  for (let i = 0; i < chars.length; i++) {
+    let c = chars[i]
+    if (c === c.toUpperCase()) { a.push(' ') }
+    a.push(c)
   }
-  return t.charAt(0).toUpperCase() + t.slice(1)
+  return a
 }
