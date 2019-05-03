dcc40d42a93866c008fc7873f9cc9abbcd92ee85
lucsan <Luke.Tennant@gmail.com>
2019-05-03 13:39:36 +0100

    Adds player and character login.

--- application.js
+++ application.js
@@ -3,10 +3,26 @@ function application() {
   const init = () => {
     autoload().loadFiles()
     document.addEventListener('chronicle_modules_loaded', modulesLoaded)
+    document.addEventListener('chronicle_player_loaded', playerLoaded)
+    document.addEventListener('chronicle_character_loaded', characterLoaded)
   }
 
   const modulesLoaded = () => {
+    marshall = marshalling()
+    marshall.props(propsPlans)
+    marshall.sets(setsPlans)
+    let stage = stageRoyal(marshall.rigging)
+    theater = theaterSpace(marshall, stage)
+    theater.buildStage()
+    player(marshall, theater).name()
+  }
 
+  const playerLoaded = () => {
+    character(marshall, theater).name()
+  }
+
+  const characterLoaded = () => {
+    marshall.character({location: 'begining'})
   }
 
   return {
